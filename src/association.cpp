65ba05db8e19374baa7bde334b5a4c17abfebca6
ximo112 <ryo.shimo112@gmail.com>
2016-01-08 18:56:24 +0900

    動的障害物の表示を重心に変更した

--- src/association.cpp
+++ src/association.cpp
@@ -10,10 +10,10 @@ using namespace std;
 class Association{
 public:
   Association(){
-    obstacle_clustering_sub = nh.subscribe("obstacle_clustering", 10, &Association::obstacle_clusteringCallBack, this);
+    objects_sub = nh.subscribe("objects", 10, &Association::objectsCallBack, this);
     associatied_obstacle_pub = nh.advertise<sensor_msgs::PointCloud>("associatied_obstacle", 1000);
   }
-  void obstacle_clusteringCallBack(const sensor_msgs::PointCloud::ConstPtr& msg){
+  void objectsCallBack(const sensor_msgs::PointCloud::ConstPtr& msg){
     callback = true;
     int count;
     float distance_max, distance;
@@ -24,65 +24,33 @@ public:
     association.channels.resize((int)msg->channels.size());
     association.channels[0].name = msg->channels[0].name;
     association.channels[0].values.resize((int)msg->channels[0].values.size());
-    association.channels[1].name = msg->channels[1].name;
-    association.channels[1].values.resize((int)msg->channels[1].values.size());
     association.header.frame_id = msg->header.frame_id;
     association.header.stamp = ros::Time::now();
     cluster_numbering_now.clear();
-    cluster_numbering_now.resize((int)msg->channels[1].values.size());
+    cluster_numbering_now.resize((int)msg->channels[0].values.size());
 
     for(int i = 0; i < (int)msg->points.size(); i++){
       association.points[i].x = msg->points[i].x;
       association.points[i].y = msg->points[i].y;
       association.channels[0].values[i] = msg->channels[0].values[i];
-      
-      association.channels[1].values[i] = msg->channels[1].values[i];
-      cluster_numbering_now[i] = msg->channels[1].values[i];
 
-      if(cluster_num < msg->channels[1].values[i] + 1){
-        cluster_num = msg->channels[1].values[i] + 1;
+      cluster_numbering_now[i] = msg->channels[0].values[i];
+
+      if(cluster_num < msg->channels[0].values[i] + 1){
+        cluster_num = msg->channels[0].values[i] + 1;
       }
     }
     cluster_x.clear();
     cluster_y.clear();
-    cluster_size.clear();
     cluster_association.clear();
     cluster_x.resize(cluster_num);
     cluster_y.resize(cluster_num);
-    cluster_size.resize(cluster_num);
     cluster_association.resize(cluster_num);
     for(int i = 0; i < cluster_num; i++){
-      cluster_x[i] = 0;
-      cluster_y[i] = 0;
-      count = 0;
-      for(int j = 0; j < (int)msg->points.size(); j++){
-        if(i == msg->channels[1].values[j]){
-          cluster_x[i] += msg->points[j].x;
-          cluster_y[i] += msg->points[j].y;
-          count += 1;
-        }
-      }
-      cluster_x[i] = cluster_x[i] / count;
-      cluster_y[i] = cluster_y[i] / count;
+      cluster_x[i] = msg->points[i].x;
+      cluster_y[i] = msg->points[i].y;
 
       cluster_association[i] = i;
-
-    }
-    for(int i = 0; i < cluster_num; i++){
-      distance_max = 0;
-      cluster_size[i] = 0;
-      for(int j = 0; j < (int)msg->points.size(); j++){
-        if(i == association.channels[1].values[j]){
-          distance = hypotf(cluster_x[i] - association.points[j].x, cluster_y[i] - association.points[j].y);
-          if(distance_max < distance){
-            distance_max = distance;
-          }
-        }
-      }
-      if(distance_max < 0.025){
-        distance_max = 0.025;
-      }
-      cluster_size[i] = pow(distance_max, 2) * M_PI;
     }
   }
 
@@ -100,10 +68,10 @@ public:
     }
 
     for(i = 0; i < association_num_max; i++){
-      if(cluster_x_old[i] == 0 && cluster_y_old[i] == 0 && cluster_size_old[i] == 0){
+      if(cluster_x_old[i] == 0 && cluster_y_old[i] == 0){
       }else{
         //
-        ROS_INFO("old:%d, cluster_x_old:%f, cluster_y_old:%f, cluster_size_old:%f", i, cluster_x_old[i], cluster_y_old[i], cluster_size_old[i]);
+        ROS_INFO("old:%d, cluster_x_old:%f, cluster_y_old:%f", i, cluster_x_old[i], cluster_y_old[i]);
 
         cov = Matrix2f::Zero();
         ave = Vector2f::Zero();
@@ -112,36 +80,21 @@ public:
         for(j = 0; j < cluster_num; j++){
           cov(0, 0) += pow(cluster_x_old[i] - cluster_x[j], 2);
           cov(1, 1) += pow(cluster_y_old[i] - cluster_y[j], 2);
-//          cov(2, 2) += pow(cluster_size_old[i] - cluster_size[j], 2);
           cov(1, 0) += (cluster_y_old[i] - cluster_y[j]) * (cluster_x_old[i] - cluster_x[j]);
-//          cov(2, 0) += (cluster_size_old[i] - cluster_size[j]) * (cluster_x_old[i] - cluster_x[j]);
-//          cov(2, 1) += (cluster_size_old[i] - cluster_size[j]) * (cluster_y_old[i] - cluster_y[j]);
         }
         cov(0, 0) = cov(0, 0) / cluster_num;
         cov(1, 1) = cov(1, 1) / cluster_num;
-//        cov(2, 2) = cov(2, 2) / cluster_num;
         cov(1, 0) = cov(1, 0) / cluster_num;
-//        cov(2, 0) = cov(2, 0) / cluster_num;
-//        cov(2, 1) = cov(2, 1) / cluster_num;
         cov(0, 1) = cov(1, 0);
-//        cov(0, 2) = cov(2, 0);
-//        cov(1, 2) = cov(2, 1);
 
         ave(0) = cluster_x_old[i];
         ave(1) = cluster_y_old[i];
-//        ave(2) = cluster_size_old[i];
 
         log_f_likelihood = -FLT_MAX;
-        for(j = 0; j < cluster_num; j++){/*
-          log_f[j] = hypotf(cluster_x[j] - cluster_x_old[i], cluster_y[j] - cluster_y_old[i]);
-          if(log_f_likelihood > log_f[j] || log_f_likelihood == -1){
-            log_f_likelihood = log_f[j];
-            association_num = j;
-          }*/
+        for(j = 0; j < cluster_num; j++){
 
           variable(0) = cluster_x[j];
           variable(1) = cluster_y[j];
-          //          variable(2) = cluster_size[j];
           variable_ave = variable - ave;
           ave_scalar = variable_ave.transpose() * cov.inverse() * variable_ave;
 
@@ -149,7 +102,7 @@ public:
           log_f[j] = log(exp(- ave_scalar / 2) / sqrt(pow(2 * M_PI, 2) * cov.determinant()));
 
           //
-          ROS_INFO("log_f:%f, x:%f, y:%f, size:%f", log_f[j], cluster_x[j], cluster_y[j], cluster_size[j]);
+          ROS_INFO("log_f:%f, x:%f, y:%f", log_f[j], cluster_x[j], cluster_y[j]);
 
           if(log_f_likelihood < log_f[j]){
             log_f_likelihood = log_f[j];
@@ -218,12 +171,7 @@ public:
         if(cluster_num_old != 0){
           likelihood();
           for(int i = 0; i < cluster_num; i++){
-            for(int j = 0; j < (int)association.points.size(); j++){
-              if(i == cluster_numbering_now[j]){
-                association.channels[1].values[j] = cluster_association[i];
-
-              }
-            }
+            association.channels[0].values[i] = cluster_association[i];
           }
           //
           ROS_INFO("-----");
@@ -239,14 +187,11 @@ public:
         }
         cluster_x_old.clear();
         cluster_y_old.clear();
-        cluster_size_old.clear();
         cluster_x_old.resize(association_num_max);
         cluster_y_old.resize(association_num_max);
-        cluster_size_old.resize(association_num_max);
         for(int i = 0; i < cluster_num_old; i++){
           cluster_x_old[cluster_association[i]] = cluster_x[i];
           cluster_y_old[cluster_association[i]] = cluster_y[i];
-          cluster_size_old[cluster_association[i]] = cluster_size[i];
         }
       }
       ros::spinOnce();
@@ -254,12 +199,12 @@ public:
   }
 private:
   ros::NodeHandle nh;
-  ros::Subscriber obstacle_clustering_sub;
+  ros::Subscriber objects_sub;
   ros::Publisher associatied_obstacle_pub;
   sensor_msgs::PointCloud association;
   vector<int> cluster_association, cluster_numbering_now;
-  vector<float> cluster_x, cluster_y, cluster_size;
-  vector<float> cluster_x_old, cluster_y_old, cluster_size_old;
+  vector<float> cluster_x, cluster_y;
+  vector<float> cluster_x_old, cluster_y_old;
   int cluster_num, cluster_num_old, association_num_max, callback;
 
 };
