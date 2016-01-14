b3c4fb35efdf64d5c6ecc4815b8a7e5aad5f5074
ximo112 <ryo.shimo112@gmail.com>
2016-01-14 21:41:51 +0900

    ver.3.1

--- src/association.cpp
+++ src/association.cpp
@@ -3,6 +3,8 @@
 #include <Eigen/Dense>
 #include <math.h>
 #include <sstream>
+#define particle_max 10
+#define range 0.1
 
 using namespace Eigen;
 using namespace std;
@@ -16,7 +18,7 @@ public:
   void objectsCallBack(const sensor_msgs::PointCloud::ConstPtr& msg){
     callback = true;
     int count;
-    float distance_max, distance;
+    float distance_max, distance, uniform1, uniform2;
     cluster_num = 0;
     association.points.clear();
     association.channels.clear();
@@ -35,11 +37,9 @@ public:
       association.channels[0].values[i] = msg->channels[0].values[i];
 
       cluster_numbering_now[i] = msg->channels[0].values[i];
-
-      if(cluster_num < msg->channels[0].values[i] + 1){
-        cluster_num = msg->channels[0].values[i] + 1;
-      }
     }
+    cluster_num = (int)msg->points.size();
+
     cluster_x.clear();
     cluster_y.clear();
     cluster_association.clear();
@@ -59,90 +59,82 @@ public:
     Vector2f ave;
     Vector2f variable;
     Vector2f variable_ave;
-    int i, j, k, count, alpha = 0, association_num;
-    float log_f[cluster_num], ave_scalar, log_f_likelihood, likelihood_value[cluster_num], diff[cluster_num], diff_min;
+    int i, j, k, count, alpha = 0, association_num, check;
+    float ave_scalar, log_f_likelihood, likelihood_value[association_num_max];
+    MatrixXf log_f;
+    log_f = MatrixXf::Zero(cluster_num_old, particle_max);
 
     for(i = 0; i < cluster_num; i++){
       cluster_association[i] = -1;
+    }
+    for(i = 0; i < cluster_num_old; i++){
       likelihood_value[i] = -FLT_MAX;
     }
 
-    for(i = 0; i < association_num_max; i++){
-      if(cluster_x_old[i] == 0 && cluster_y_old[i] == 0){
-      }else{
-        //
-        ROS_INFO("old:%d, cluster_x_old:%f, cluster_y_old:%f", i, cluster_x_old[i], cluster_y_old[i]);
+    for(i = 0; i < cluster_num; i++){
+      ROS_INFO("now:%d, cluster_x:%f, cluster_y:%f", i, cluster_x[i], cluster_y[i]);////
 
-        cov = Matrix2f::Zero();
-        ave = Vector2f::Zero();
-        variable = Vector2f::Zero();
-        variable_ave = Vector2f::Zero();
-        for(j = 0; j < cluster_num; j++){
-          cov(0, 0) += pow(cluster_x_old[i] - cluster_x[j], 2);
-          cov(1, 1) += pow(cluster_y_old[i] - cluster_y[j], 2);
-          cov(1, 0) += (cluster_y_old[i] - cluster_y[j]) * (cluster_x_old[i] - cluster_x[j]);
+      cov = Matrix2f::Zero();
+      ave = Vector2f::Zero();
+      variable = Vector2f::Zero();
+      variable_ave = Vector2f::Zero();
+      for(j = 0; j < cluster_num_old; j++){
+        for(k = 0; k < particle_max; k++){
+          cov(0, 0) += pow(cluster_x[i] - particle_x(j, k), 2);
+          cov(1, 1) += pow(cluster_y[i] - particle_y(j, k), 2);
+          cov(1, 0) += (cluster_x[i] - particle_x(j, k)) * (cluster_y[i] - particle_y(j, k));
         }
-        cov(0, 0) = cov(0, 0) / cluster_num;
-        cov(1, 1) = cov(1, 1) / cluster_num;
-        cov(1, 0) = cov(1, 0) / cluster_num;
-        cov(0, 1) = cov(1, 0);
+      }
+      cov(0, 0) = cov(0, 0) / (cluster_num_old * particle_max);
+      cov(1, 1) = cov(1, 1) / (cluster_num_old * particle_max);
+      cov(1, 0) = cov(1, 0) / (cluster_num_old * particle_max);
+      cov(0, 1) = cov(1, 0);
 
-        ave(0) = cluster_x_old[i];
-        ave(1) = cluster_y_old[i];
+      ave(0) = cluster_x[i];
+      ave(1) = cluster_y[i];
 
-        log_f_likelihood = -FLT_MAX;
-        for(j = 0; j < cluster_num; j++){
-
-          variable(0) = cluster_x[j];
-          variable(1) = cluster_y[j];
+      log_f_likelihood = -FLT_MAX;
+      for(j = 0; j < cluster_num_old; j++){
+        for(k = 0; k < particle_max; k++){
+          variable(0) = particle_x(j, k);
+          variable(1) = particle_y(j, k);
           variable_ave = variable - ave;
           ave_scalar = variable_ave.transpose() * cov.inverse() * variable_ave;
 
           //多変量正規分布
-          log_f[j] = log(exp(- ave_scalar / 2) / sqrt(pow(2 * M_PI, 2) * cov.determinant()));
+          log_f(j, k) = log(exp(- ave_scalar / 2) / sqrt(pow(2 * M_PI, 2) * cov.determinant()));
 
-          //
-          ROS_INFO("log_f:%f, x:%f, y:%f", log_f[j], cluster_x[j], cluster_y[j]);
+          ROS_INFO("log_f:%f, x:%f, y:%f, num:%d", log_f(j, k), particle_x(j, k), particle_y(j, k), cluster_association_old[j]);
 
-          if(log_f_likelihood < log_f[j]){
-            log_f_likelihood = log_f[j];
-            association_num = j;
+          if(log_f_likelihood < log_f(j, k)){
+            log_f_likelihood = log_f(j, k);
+            association_num = cluster_association_old[j];
           }
         }
-        //
-        ROS_INFO("aso_num:%d, log_f_likelihood:%f", association_num, log_f_likelihood);
-
-        if(log_f_likelihood != -FLT_MAX){
-
-          if(cluster_association[association_num] == -1){
-            cluster_association[association_num] = i;
-            likelihood_value[association_num] = log_f_likelihood;
-          }else{
-            if(likelihood_value[association_num] < log_f_likelihood){
-              cluster_association[association_num] = i;
-              likelihood_value[association_num] = log_f_likelihood;
-            }
-          }
-
-        }else{
-
-          diff_min = -1;
-          for(j = 0; j < cluster_num; j++){
-            diff[j] = hypotf(cluster_x[j] - cluster_x_old[i], cluster_y[j] - cluster_y_old[i]);
-            if(diff_min > diff[j] || diff_min == -1){
-              diff_min = diff[j];
-              association_num = j;
-            }
-          }
-          cluster_association[association_num] = i;
-          likelihood_value[association_num] = FLT_MAX;
-        }
-
       }
-    }
-    //
-    for(j = 0; j < cluster_num; j++){
-      ROS_INFO("now:%d, old:%d, diff:%f", j, cluster_association[j], likelihood_value[j]);
+
+      ROS_INFO("log_f_likelihood:%f, num:%d", log_f_likelihood, association_num);
+
+      check = false;
+      for(j = 0; j < cluster_num; j++){
+        if(association_num == cluster_association[j]){
+          check = true;
+        }
+      }
+
+      if(likelihood_value[association_num] == -FLT_MAX){
+        cluster_association[i] = association_num;
+        likelihood_value[association_num] = log_f_likelihood;
+      }else if(likelihood_value[association_num] < log_f_likelihood){
+        ROS_WARN("%f", log_f_likelihood);////
+        for(j = 0; j < cluster_num; j++){
+          if(association_num == cluster_association[j]){
+            cluster_association[j] == -1;
+          }
+        }
+        cluster_association[i] = association_num;
+        likelihood_value[association_num] = log_f_likelihood;
+      }
     }
 
     for(i = 0; i < cluster_num; i++){
@@ -160,10 +152,14 @@ public:
         }
       }
     }
+    for(i = 0; i < cluster_num; i++){
+      ROS_INFO("now:%d, asso:%d", i, cluster_association[i]);////
+    }
   }
 
   void run(){
     callback = false;
+    cluster_num_old = 0;
     float hoge_x, hoge_y;
     while(ros::ok()){
       if(callback == true){
@@ -178,6 +174,7 @@ public:
 
           associatied_obstacle_pub.publish(association);
         }
+
         cluster_num_old = cluster_num;
         association_num_max = 0;
         for(int i = 0; i < cluster_num_old; i++){
@@ -185,13 +182,18 @@ public:
             association_num_max = cluster_association[i] + 1;
           }
         }
-        cluster_x_old.clear();
-        cluster_y_old.clear();
-        cluster_x_old.resize(association_num_max);
-        cluster_y_old.resize(association_num_max);
+        cluster_association_old.clear();
+        cluster_association_old.resize(cluster_num_old);
+        particle_x = MatrixXf::Zero(cluster_num_old, particle_max);
+        particle_y = MatrixXf::Zero(cluster_num_old, particle_max);
         for(int i = 0; i < cluster_num_old; i++){
-          cluster_x_old[cluster_association[i]] = cluster_x[i];
-          cluster_y_old[cluster_association[i]] = cluster_y[i];
+          for(int j = 0; j < particle_max; j++){
+            float uniform1 = ((float)rand() + 1) / ((float)RAND_MAX + 2);
+            float uniform2 = ((float)rand() + 1) / ((float)RAND_MAX + 2);
+            particle_x(i, j) = sqrt(-2 * log(uniform1)) * cos(2 * M_PI * uniform2) * range + cluster_x[i];
+            particle_y(i, j) = sqrt(-2 * log(uniform1)) * sin(2 * M_PI * uniform2) * range + cluster_y[i];
+          }
+          cluster_association_old[i] = cluster_association[i];
         }
       }
       ros::spinOnce();
@@ -202,10 +204,10 @@ private:
   ros::Subscriber objects_sub;
   ros::Publisher associatied_obstacle_pub;
   sensor_msgs::PointCloud association;
-  vector<int> cluster_association, cluster_numbering_now;
+  vector<int> cluster_association, cluster_numbering_now, cluster_association_old;
   vector<float> cluster_x, cluster_y;
-  vector<float> cluster_x_old, cluster_y_old;
   int cluster_num, cluster_num_old, association_num_max, callback;
+  MatrixXf particle_x, particle_y;
 
 };
 
